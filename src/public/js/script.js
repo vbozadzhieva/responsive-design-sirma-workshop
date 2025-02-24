c0c16d3a2c77658aab464e6429da1862d8aaa34a
Vanya <vbozadzhieva29@gmail.com>
2025-02-24 17:01:55 +0200

    reset burger menu and nav on media change

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -1,20 +1,31 @@
 function initEventListeners() {
-
     const burgerMenuElement = document.querySelector('.burger-menu');
     burgerMenuElement.addEventListener('click', onBurgerMenuClick);
 
-    function onBurgerMenuClick() {
-        const navContainerElement = document.querySelector('.nav-container');
-        const burgerMenuElement = document.querySelector('.burger-menu');
+    const mobileMediaQuery = window.matchMedia('(max-width: 768px)');
+    mobileMediaQuery.addEventListener('change', onMediaChange);
+}
 
-        // toggle burger menu icon and navigation
-        if (burgerMenuElement.classList.contains('open-menu')) {
-            burgerMenuElement.classList.replace('open-menu', 'close-menu');
-            navContainerElement.style.display = 'block';
-        } else {
-            burgerMenuElement.classList.replace('close-menu', 'open-menu');
-            navContainerElement.style.display = '';
-        }
+// reset burger menu and nav on media change
+function onMediaChange() {
+    const burgerMenuElement = document.querySelector('.burger-menu');
+    const navContainerElement = document.querySelector('.nav-container');
+
+    navContainerElement.style.display = '';
+    burgerMenuElement.classList.replace('close-menu', 'open-menu');
+}
+
+function onBurgerMenuClick() {
+    const navContainerElement = document.querySelector('.nav-container');
+    const burgerMenuElement = document.querySelector('.burger-menu');
+
+    // toggle burger menu icon and navigation
+    if (burgerMenuElement.classList.contains('open-menu')) {
+        burgerMenuElement.classList.replace('open-menu', 'close-menu');
+        navContainerElement.style.display = 'block';
+    } else {
+        burgerMenuElement.classList.replace('close-menu', 'open-menu');
+        navContainerElement.style.display = '';
     }
 }
 
